28b9ce7573720f6e9d1ff096ad3ca814b93f1c6b
AdeleGropplero <adele.gdt@hotmail.com>
2024-11-13 18:04:55 +0100

    fine, esclusi extra

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* Qui ci riscriviamo la funzione per la creazione del carrello */
+
 const fetchBooks = function () {
   fetch("https://striveschool-api.herokuapp.com/books")
     .then((responses) => {
@@ -5,6 +7,8 @@ const fetchBooks = function () {
 
       if (responses.ok) {
         return responses.json();
+      } else {
+        throw new Error("errore nel fetch");
       }
     })
     .then((books) => {
@@ -19,11 +23,20 @@ const fetchBooks = function () {
 
         const card = document.createElement("div");
         card.className =
-          "card border border-light-subtle border-4 mb-3 bg-black";
-        card.style.width = "376px";
+          "card border border-light-subtle border-4 mb-3 bg-black ";
+
+        card.style.height = "760px";
+
+        const divImg = document.createElement("div");
+        divImg.style.width = "368px";
+        divImg.style.height = "510px";
 
         const img = document.createElement("img");
-        img.style.width = "100%";
+        img.style.width = "90%";
+        img.style.height = "100%";
+        img.style.overflow = "hidden";
+        img.style.objectFit = "cover";
+        img.style.paddingTop = "20px";
         img.src = book.img;
         img.className = "card-img-top";
 
@@ -49,14 +62,39 @@ const fetchBooks = function () {
 
         row.appendChild(col);
         col.appendChild(card);
-        card.appendChild(img);
+        card.appendChild(divImg);
+        divImg.appendChild(img);
         card.appendChild(cardBody);
         cardBody.appendChild(title);
         cardBody.appendChild(price);
         cardBody.appendChild(button);
         cardBody.appendChild(button2);
+
+        button2.onclick = function (e) {
+          e.preventDefault();
+          col.remove();
+        };
+
+        button.onclick = function (e) {
+          e.preventDefault();
+
+          const ul = document.getElementById("ul");
+          const li = document.createElement("li");
+          li.innerText = `${book.title} - ${book.price} $`;
+          li.className = "list-group-item p-3  ";
+          ul.appendChild(li);
+
+          let carrello = localStorage.getItem("carrello")
+            ? JSON.parse(localStorage.getItem("carrello"))
+            : [];
+
+          carrello.push({ title: book.title, price: book.price });
+
+          localStorage.setItem("carrello", JSON.stringify(carrello));
+        };
       });
-    });
+    })
+    .catch((err) => console.log(err));
 };
 
 window.onload = () => {
