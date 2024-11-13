e12ea102a43a2da0df4eaef14de22e6d6f054786
AdeleGropplero <adele.gdt@hotmail.com>
2024-11-13 16:24:55 +0100

    migliorie

--- script.js
+++ script.js
@@ -35,13 +35,17 @@ const fetchBooks = function () {
         title.innerText = `${book.title}`;
 
         const price = document.createElement("p");
-        price.className = "card-text";
+        price.className = "card-text bg-dark text-info";
         price.innerText = `${book.price} $`;
-        price.style.color = "red";
+        price.style.margin = "50px";
 
-        const button = document.createElement("a");
-        button.className = "btn btn-success";
+        const button = document.createElement("button");
+        button.className = "btn btn-success me-3";
         button.innerText = "+ Aggiungi al carrello";
+
+        const button2 = document.createElement("button");
+        button2.className = "btn btn-danger";
+        button2.innerText = "scarta";
 
         row.appendChild(col);
         col.appendChild(card);
@@ -50,6 +54,7 @@ const fetchBooks = function () {
         cardBody.appendChild(title);
         cardBody.appendChild(price);
         cardBody.appendChild(button);
+        cardBody.appendChild(button2);
       });
     });
 };
